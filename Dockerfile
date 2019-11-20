bdc0c19a8487eaa62466c4c763dd658cfda47927
mgramin <mgramin@gmail.com>
2019-11-20 16:03:31 +0400

    init

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM mgramin/sql-boot
+RUN curl https://jdbc.postgresql.org/download/postgresql-42.2.8.jar -o postgresql-jdbc.jar
---
+++ sql/hello_world.sql
@@ -0,0 +1,7 @@
+/*{
+  "name": "hello_world_sql_script",
+  "title": "Hello World SQL-script",
+  "tags": "ui"
+}*/
+select generate_series as value
+  from generate_series(1, 100)
